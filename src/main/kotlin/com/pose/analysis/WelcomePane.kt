dae532073e925343a2bc302cba49c4dc902a73eb
XD5000 <teammoorefamily@gmail.com>
2023-12-27 09:48:15 -0800

    Cleaned up the WelcomePane

--- src/main/kotlin/com/pose/analysis/WelcomePane.kt
+++ src/main/kotlin/com/pose/analysis/WelcomePane.kt
@@ -10,47 +10,54 @@ import javafx.scene.layout.StackPane
 
 object WelcomePane: StackPane() {
 
-    val welcomeText = Label("Welcome to PoseAnalysis!")
-
-    val helpText = Label("For a tutorial, click help -> intro")
-
-    val startText = Label("To get started, click file -> open, then choose video, pose, or animation")
-
-    val infoText = Label("For more information, click help -> readme")
+    // Sets up the 4 lines of the main welcome text
+    private val welcomeText = Label("Welcome to PoseAnalysis!")
+    private val helpText = Label("For a tutorial, click Help -> Intro")
+    private val startText = Label("To get started, click File -> Open, then choose Video, Pose, or Animation")
+    private val infoText = Label("For more information, click Help -> ReadMe")
 
     init {
 
-
+        // Makes sure the text is aligned to the top-center of the pane
         WelcomePane.alignment = Pos.TOP_CENTER
 
+        // Sets the font, color, and position of the first line
         welcomeText.font = largeFont
         welcomeText.setColor(mutedTextColor)
         welcomeText.translateY = screenHeight * 0.125
 
+        // Sets the font, color, and position of the second line
         helpText.font = mediumFont
         helpText.setColor(mutedTextColor)
         helpText.translateY = screenHeight * 0.5
 
+        // Sets the font, color, and position of the third line
         startText.font = mediumFont
         startText.setColor(mutedTextColor)
         startText.translateY = screenHeight * 0.625
 
+        // Sets the font, color, and position of the fourth line
         infoText.font = mediumFont
         infoText.setColor(mutedTextColor)
         infoText.translateY = screenHeight * 0.75
 
+        // Adds every line to the pane
         WelcomePane.children.addAll(
 
             welcomeText,
             helpText,
             startText,
             infoText
+
         )
 
+        // Makes sure that the pane is centered.
+        // When the width of the pane changes, it re-centers itself
         WelcomePane.widthProperty().addListener { _, _, newWidth ->
             WelcomePane.layoutX = (MainPane.screenWidth / 2.0) - (newWidth.toDouble() / 2.0)
         }
 
+        // Makes sure the pane is only visible when it needs to be
         WelcomePane.visibleProperty().bind(
             isWelcome
         )
